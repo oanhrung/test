36a3dd9507bdfb95da03c970df6892b5e18a1368
root <oanhrung@gmail.com>
2017-09-19 14:15:44 +0700

    add new project

---
+++ git1/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: oanhntk
+ *
+ * Created on September 19, 2017, 2:13 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
